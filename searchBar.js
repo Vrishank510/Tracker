e25f08133b9b2bd6b5af607b9393b79d4d9e3e0a
Vrishank <vrish.reddy@gmail.com>
2023-03-26 07:16:55 -0700

    second commit

--- searchBar.js
+++ searchBar.js
@@ -1,7 +1,34 @@
 let counter = 0;
-const buttonClick = document.getElementById("button1");
+let shirtCounter = 0;
+let pantCounter = 0;
+let sheetCounter = 0;
+let d;
+const buttonClick = [document.getElementById("button1"),document.getElementById("button3"),document.getElementById("button2")];
 const totalOrders = document.getElementById("totalOrders");
-buttonClick.onclick = ()=>{
-    counter++;
+const totalPrice = document.getElementById("totalPrice");
+
+for(var i=0;i<3;i++)
+{
+    if (d === 0)
+        {
+            shirtCounter+=30;
+        }
+    else if (d === 1)
+        {
+            sheetCounter+=50;
+        }
+    else if (d === 2)
+        {
+            pantCounter+=40;
+        }
+    buttonClick[i].addEventListener('click',()=>{
+        console.log(i);
+        d = i;
+        counter++;
+        
+        
+        totalOrders.textContent = counter;
+        let tp = shirtCounter+pantCounter+sheetCounter;
+        totalPrice.textContent = "Rs " + tp;
+    })
 }
-totalOrders.textContent = counter;
